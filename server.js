98abe447be7856c25904de89883d4f2d48c322f1
Eric Gustafson <weuxfya@gmail.com>
2019-12-02 08:59:37 -0700

    Update root pwd

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'hmtdih2tym!',
+    password: 'root',
     database: 'employee_db',
     multipleStatements: true
 });
