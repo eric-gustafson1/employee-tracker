f371bbe0324822fa1c2d34ec22c75eb345f43ff0
Eric Gustafson <weuxfya@gmail.com>
2019-12-06 15:31:31 -0700

    Add Heroku config for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 const mysql = require('mysql');
 const chalk = require('chalk');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'employee_db',
-    multipleStatements: true
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'employee_db',
+        multipleStatements: true
+    });
+};
 
 connection.connect((err) => {
     if (err) {
