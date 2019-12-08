f52af40ec3e9b4fba76088d36739bbf1ca658136
Eric Gustafson <weuxfya@gmail.com>
2019-12-08 08:32:30 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,15 @@ const mysql = require('mysql');
 const chalk = require('chalk');
 
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
 
-    connection = mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'root',
-        database: 'employee_db',
-        multipleStatements: true
-    });
-};
+connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'employee_db',
+    multipleStatements: true
+});
 
 connection.connect((err) => {
     if (err) {
